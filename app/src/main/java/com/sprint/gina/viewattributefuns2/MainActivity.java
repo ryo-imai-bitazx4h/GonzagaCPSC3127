93ac8922805a59da9dcf5f846c60476edfa6fc6b
ryo-imai-bitazx4h <jiaotaozhuo3@126.com>
2021-03-22 22:04:37 +0800

    finish attributes

--- app/src/main/java/com/sprint/gina/viewattributefuns2/MainActivity.java
+++ app/src/main/java/com/sprint/gina/viewattributefuns2/MainActivity.java
@@ -3,6 +3,8 @@ package com.sprint.gina.viewattributefuns2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String onText = getString(R.string.on);
+                String offText = getString(R.string.off);
+                if (button.getText().toString().compareTo(onText) == 0) {
+                    button.setText(offText);
+                }
+                else {
+                    button.setText(onText);
+                }
+            }
+        });
     }
 }
